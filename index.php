b5b7aa23e75a0821194c09fb31ade9e003a0983e
MariaDevieCahyani <cahyanimariadevie12@gmail.com>
2024-04-23 10:08:15 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<h1>list website praktikum</h1>
+<li>
+    <a href="praktek1.php">praktek 1</a></li>
+    <li><a href="praktek2.php">praktek 2</a></li>
+    <li><a href="praktek3.php">praktek 3</a></li>
+    <li><a href="praktek4.php">praktek 4</a></li>
+    <li><a href="praktek5.php">praktek 5</a></li>
+    <li><a href="praktek6.php">praktek 6</a></li>
+    <li><a href="praktek7.php">praktek 7</a></li>
+    <li><a href="praktek8.php">praktek 8</a></li>
+    <li><a href="praktek9.php">praktek 9</a></li>
+    <li><a href="praktek10.php">praktek 10</a></li>
+    <li><a href="praktek11.php">praktek 11</a></li>
+    <li><a href="praktek12.php">praktek 12</a></li>
+    <li><a href="praktek13.php">praktek 13</a></li>
+    <li><a href="praktek14.php">praktek 14</a></li>
+    <li><a href="praktek15.php">praktek 15</a></li>
+    <li><a href="praktek16.php">praktek 16</a></li>
+    <li><a href="praktek17.php">praktek 17</a></li>
+    <li><a href="praktek18.php">praktek 18</a></li>
+    </li>
